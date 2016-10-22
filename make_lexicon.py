aede6daebffa13e1f8eeea4db8c9cbfde0435b31
mjlavin80 <lavin@pitt.edu>
2016-10-22 12:46:30 -0400

    lexicon

---
+++ make_lexicon.py
@@ -0,0 +1,25 @@
+from application import db
+from application.models import *
+import pymysql
+from config import USER, PWD
+
+#Make empty dictionary
+terms = {}
+#Loop ids from metadata
+_ids = [i.id for i in db.session.query(Metadata).all()]
+
+conn = pymysql.connect(host='localhost', port=3306, user=USER, passwd=PWD, db='horror')
+
+cur = conn.cursor()
+
+for _id in _ids[:5]:
+    print(_id)
+    #loop terms matching certain criteria (regex query)
+    a = cur.execute(query)
+    print(a)
+    #Try to add to count in dictionary
+    #Except add term to dictionary with count of one
+    #At end of loop, convert to pandas df (terms and counts)
+    #Order by count
+    #Stop at 10k
+    #Save as csv in lavin_lexicon folder
