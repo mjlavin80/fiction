d5eba56835b5ea34612a318af145ea9a72daaaa2
root <root@js-171-191.jetstream-cloud.org>
2016-10-22 14:50:36 -0400

    meanshift some errors corrected

--- meanshift.py
+++ meanshift.py
@@ -1,8 +1,4 @@
 
-# coding: utf-8
-
-# In[10]:
-
 from nltk import corpus
 import numpy as np
 from sklearn.cluster import MeanShift, estimate_bandwidth
@@ -10,27 +6,27 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 import pandas as pd
 
-
-# In[14]:
-
 stops = corpus.stopwords.words('english') 
 # load features
 feature_df = pd.read_csv("lavin_lexicon/features_all.csv")
 features_all = [i for i in list(feature_df["term"]) if i not in stops]
 features = features_all[:500]
 
-
-# In[ ]:
-
 from application import db
 from application.models import *
 import pymysql
 from collections import Counter
 from config import USER, PWD
+import pymysql
 
 # loop ids, store order here
 _ids  = [i.id for i in db.session.query(Metadata).all()]
+
 feature_dicts = []
+
+conn = pymysql.connect(host='localhost', port=3306, user=USER, passwd=PWD, db='horror')
+cur = conn.cursor()
+
 for _id in _ids:
     feature_dict = {}
     # get types and counts
@@ -41,6 +37,7 @@ for _id in _ids:
         #add to dict if ok to use
         if row[0] in features:
             feature_dict[row[0]] = row[1]
+    feature_dicts.append(feature_dict)
         
 # create vectors using N top features not in stops
 tfidf = TfidfTransformer()
@@ -48,7 +45,6 @@ vec = DictVectorizer()
 vect = vec.fit_transform(feature_dicts)
 adjusted = tfidf.fit_transform(vect)
 data = adjusted.toarray()
-data
 
 # the following bandwidth can be automatically detected using
 bandwidth = estimate_bandwidth(data, quantile=0.2, n_samples=200)
@@ -64,5 +60,7 @@ n_clusters_ = len(labels_unique)
 print(labels)
 print("number of estimated clusters : %d" % n_clusters_)
 
+cur.close()
+conn.close()
 #Store results for graphing
 
