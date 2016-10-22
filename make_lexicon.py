3f310aff2d1ff5d0404759b3e85c399bff1a5c99
mjlavin80 <lavin@pitt.edu>
2016-10-22 13:09:37 -0400

    lexicon

--- make_lexicon.py
+++ make_lexicon.py
@@ -1,8 +1,9 @@
 from application import db
 from application.models import *
 import pymysql
+from collections import Counter
 from config import USER, PWD
-
+import pandas as pd
 #Make empty dictionary
 terms = {}
 #Loop ids from metadata
@@ -18,12 +19,22 @@ for _id in _ids[:5]:
     #loop terms matching certain criteria (regex query)
     a = cur.execute(query)
     for row in cur:
-        print(row[0])
+        #Try to add to count in dictionary
+        try:
+            current = terms[row[0]]
+            terms[row[0]] = current+1
+        except:
+            #Except add term to dictionary with count of one
+            terms[row[0]] = 1
+
+#At end of loop, order by count, stop at 10k
+term_pairs = Counter(terms).most_common(10000)
+
+#convert to pandas df (terms and counts)
+df = pd.DataFrame(term_pairs, columns=["term", "docs"])
+
+#Save as csv in lavin_lexicon folder
+df.to_csv("lavin_lexicon/features_all.csv")
+
 cur.close()
 conn.close()
-    #Try to add to count in dictionary
-    #Except add term to dictionary with count of one
-    #At end of loop, convert to pandas df (terms and counts)
-    #Order by count
-    #Stop at 10k
-    #Save as csv in lavin_lexicon folder
